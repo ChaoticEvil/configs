7afb6bf741bea2fe6ba4bbd60995913775b0593c
Neuromantic <peter.brovchenko@gmail.com>
2015-12-08 09:49:43 +0300

    Добавил крутые твики для разработки на Python из под Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -263,6 +263,20 @@
 '(flycheck-perl-include-path
   (quote
    ("/usr/local/share/perl/5.20.2/")))
+
+;; Python
+(elpy-enable)
+(elpy-use-ipython)
+
+;; use flycheck not flymake with elpy
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; enable autopep8 formatting on save
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; End | Plugins settings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
