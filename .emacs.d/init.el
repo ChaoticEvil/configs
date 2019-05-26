07f78c8e89732f7dff2e14e6f1434c6a36198931
ChaoticEvil <peter.brovchenko@gmail.com>
2019-05-26 10:39:13 +0300

    typo

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -642,78 +642,21 @@
     :config
     (add-hook 'scala-mode-hook 'ensime-scala-mode-hook))
 
-(setq exec-path (append exec-path (list "/usr/local/bin/scala" ))) ;; Set location of scala bin
+(use-package scala-mode
+    :ensure t
+    :interpreter
+    ("scala" . scala-mode)
+    :config
+    (setq scala-indent:use-javadoc-style t))
 
+(use-package sbt-mode
+    :commands sbt-start sbt-command
+    :config
+    (substitute-key-definition
+     'minibuffer-complete-word
+     'self-insert-command
+     minibuffer-local-completion-map))
 
-;; (defun my-web-mode-hook ()
-;;   "Hooks for Web mode."
-;;   (setq web-mode-enable-current-element-highlight t)
-;;   (setq web-mode-ac-sources-alist
-;;         '(("css" . (ac-source-css-property))
-;;           ("html" . (ac-source-words-in-buffer ac-source-abbrev)))
-;;   )
-;; )
-;; (add-hook 'web-mode-hook  'my-web-mode-hook)
-
-;; ;; Javascript
-;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-;; (add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
-;; (add-hook 'js-mode-hook 'js2-minor-mode)
-;; (add-hook 'js2-mode-hook 'ac-js2-mode)
-;; (setq js2-highlight-level 4)
-;; (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-
-;; ;; Flycheck
-;; (require 'flycheck)
-;; (add-hook 'after-init-hook #'global-flycheck-mode) ;; turn on flychecking globally
-
-;; ;; Disable jshint since we prefer eslint checking
-;; (setq-default flycheck-disabled-checkers
-;;   (append flycheck-disabled-checkers
-;;     '(javascript-jshint)))
-
-;; (flycheck-add-mode 'javascript-eslint 'web-mode) ;; use eslint with web-mode for jsx files
-
-;; (setq-default flycheck-temp-prefix ".flycheck") ;; customize flycheck temp file prefix
-
-;; ;; Disable json-jsonlist checking for json files
-;; (setq-default flycheck-disabled-checkers
-;;   (append flycheck-disabled-checkers
-;;     '(json-jsonlist)))
-
-;; ;; Check html and css
-;; (with-eval-after-load 'flycheck
-;;   (flycheck-add-mode 'html-tidy 'web-mode)
-;;   (flycheck-add-mode 'css-csslint 'web-mode))
-
-;; ;; For better jsx syntax-highlighting in web-mode
-;; (defadvice web-mode-highlight-part (around tweak-jsx activate)
-;;   (if (equal web-mode-content-type "jsx")
-;;     (let ((web-mode-enable-part-face nil))
-;;       ad-do-it)
-;;     ad-do-it))
-
-;; (add-hook 'js2-mode (lambda ()
-;; 	((global-unset-key (kbd "M-j"))
-;; 	 (global-set-key (kbd "M-j") 'backward-char)
-;; 	 )))
-
-;; ;; Python
-;; (elpy-enable)
-;; (setq python-shell-interpreter "ipython"
-;;       python-shell-interpreter-args "-i --simple-prompt")
-
-;; ;; Use flycheck not flymake with elpy
-;; (when (require 'flycheck nil t)
-;;   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-;;   (add-hook 'elpy-mode-hook 'flycheck-mode))
-
-;; ;; Enable autopep8 formatting on save
-;; (require 'py-autopep8)
-;; (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-
-;; ;; Ensime
-;; (require 'ensime)
-;; (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+(setq exec-path (append exec-path (list "/usr/share/scala/bin" ))) ;; Set location of scala bin
 
 ;;; init.el ends here
