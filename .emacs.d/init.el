9ed342ab35abc7bb7bae6eeea88a6a8e95cfbfa9
ChaoticEvil <peter.brovchenko@gmail.com>
2020-10-02 06:36:33 +0300

    scala-mode, lps-scala, treemacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -745,11 +745,6 @@
 ;; Scala
 ;;
 
-;; (use-package ensime
-;;     :ensure t
-;;     :config
-;;     (add-hook 'scala-mode-hook 'ensime-scala-mode-hook))
-
 (use-package scala-mode
     :ensure t
     :interpreter
@@ -757,16 +752,135 @@
     :config
     (setq scala-indent:use-javadoc-style t))
 
-;; (use-package sbt-mode
-;;     :commands sbt-start sbt-command
-;;     :config
-;;     (substitute-key-definition
-;;      'minibuffer-complete-word
-;;      'self-insert-command
-;;      minibuffer-local-completion-map))
+(use-package sbt-mode
+    :ensure t
+    :commands sbt-start sbt-command
+    :config
+    (substitute-key-definition
+     'minibuffer-complete-word
+     'self-insert-command
+     minibuffer-local-completion-map)
+    (setq sbt:program-options '("-Dsbt.supershell=false")))
 
-;; ;; Set location of scala bin
-;; (setq exec-path (append exec-path (list "/usr/share/scala/bin" )))
+(use-package lsp-mode
+    :ensure t
+    :hook  (scala-mode . lsp)
+    (lsp-mode . lsp-lens-mode)
+    :config
+    (setq lsp-prefer-flymake nil)
+    (add-hook 'scala-mode-hook #'lsp)
+    (setq lsp-print-performance t)
+    (setq company-backends '(company-capf))
+    )
+
+;; Add metals backend for lsp-mode
+(use-package lsp-metals
+    :ensure t
+    :config (setq lsp-metals-treeview-show-when-views-received t))
+
+(use-package lsp-ui
+    :ensure t)
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay      0.5
+          treemacs-directory-name-transformer    #'identity
+          treemacs-display-in-side-window        t
+          treemacs-eldoc-display                 t
+          treemacs-file-event-delay              5000
+          treemacs-file-extension-regex          treemacs-last-period-regex-value
+          treemacs-file-follow-delay             0.2
+          treemacs-file-name-transformer         #'identity
+          treemacs-follow-after-init             t
+          treemacs-git-command-pipe              ""
+          treemacs-goto-tag-strategy             'refetch-index
+          treemacs-indentation                   2
+          treemacs-indentation-string            " "
+          treemacs-is-never-other-window         nil
+          treemacs-max-git-entries               5000
+          treemacs-missing-project-action        'ask
+          treemacs-move-forward-on-expand        nil
+          treemacs-no-png-images                 nil
+          treemacs-no-delete-other-windows       t
+          treemacs-project-follow-cleanup        nil
+          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                      'left
+          treemacs-recenter-distance             0.1
+          treemacs-recenter-after-file-follow    nil
+          treemacs-recenter-after-tag-follow     nil
+          treemacs-recenter-after-project-jump   'always
+          treemacs-recenter-after-project-expand 'on-distance
+          treemacs-show-cursor                   nil
+          treemacs-show-hidden-files             t
+          treemacs-silent-filewatch              nil
+          treemacs-silent-refresh                nil
+          treemacs-sorting                       'alphabetic-asc
+          treemacs-space-between-root-nodes      t
+          treemacs-tag-follow-cleanup            t
+          treemacs-tag-follow-delay              1.5
+          treemacs-user-mode-line-format         nil
+          treemacs-user-header-line-format       nil
+          treemacs-width                         35
+          treemacs-workspace-switch-cleanup      nil)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :ensure t)
+
+(use-package treemacs-persp ;;treemacs-persective if you use perspective.el vs. persp-mode
+  :after treemacs persp-mode ;;or perspective vs. persp-mode
+  :ensure t
+  :config (treemacs-set-scope-type 'Perspectives))
+
+(use-package posframe
+    :ensure t)
+
+(use-package dap-mode
+    :ensure t
+    :hook
+    (lsp-mode . dap-mode)
+    (lsp-mode . dap-ui-mode))
+
+;; Set location of scala bin
+(setq exec-path (append exec-path (list "/usr/local/bin" )))
 
 ;;
 ;; /Scala
@@ -838,7 +952,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(alert pyenv-mode pyenv elpy company-lsp lsp-ui lsp-mode scala-mode lua-mode js2-mode flycheck-irony nyan-mode org-bullets flycheck company-restclient restclient highlight-symbol expand-region crux pomidor yaml-mode markdown-mode web-mode magit dumb-jump rainbow-delimiters company-irony company yasnippet-snippets yasnippet nimbus-theme use-package)))
+   '(pyenv-mode elpy treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile lsp-ui lsp-metals lsp-mode sbt-mode scala-mode lua-mode js2-mode flycheck-irony nyan-mode org-bullets flycheck company-restclient restclient highlight-symbol expand-region crux pomidor yaml-mode markdown-mode web-mode magit dumb-jump rainbow-delimiters company-irony company yasnippet-snippets yasnippet nimbus-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
